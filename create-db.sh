f6defa7dbc9af53018f075c07aae7a091c2ebd3d
Morgawr <morgawr@gmail.com>
2014-05-13 21:28:35 +0200

    suppressed useless errors in db setup

--- create-db.sh
+++ create-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir db
-rm db/database.db
+mkdir db 2> /dev/null
+rm db/database.db 2> /dev/null
 sqlite3 db/database.db << EOF
 create table data(id integer primary key autoincrement, text longtext, type char(5), policy char(20), expires_at timestamp, max_visits int, visits int);
 EOF
