1c0717ef0bd071ed2c737aeb289caf9bb219fc9d
Federico "Morg" Pareschi <morgawr@gmail.com>
2021-02-14 16:14:04 +0900

    Remove non-ASCII words from wordlist file creation

--- build-wordlist.sh
+++ build-wordlist.sh
@@ -8,7 +8,7 @@ DICTFILE=/usr/share/dict/words
 OUTFILE=db/wordlist
 if test -f "$DICTFILE"; then
 	# Get all words longer than 5 characters and save them in their own wordlist file
-	awk 'length>5' $DICTFILE | uniq > $OUTFILE
+	awk 'length>5' $DICTFILE | LC_CTYPE=C awk '! /[^[:alnum:]]/' | uniq > $OUTFILE
 else
 	(>&2 echo "Error: The file ${DICTFILE} does not exist. Cannot generate wordlist for file indexing.")
 	exit 1
